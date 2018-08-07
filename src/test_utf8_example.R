28f60d84957c6e7cf1b66507146de9ff301d6db0
damianooldoni <damiano.oldoni@inbo.be>
2018-08-07 16:23:06 +0200

    Add extra point

--- src/test_utf8_example.R
+++ src/test_utf8_example.R
@@ -8,12 +8,13 @@ Sys.getlocale(category = "LC_CTYPE")
 string_utf8 <- "¡I¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶"
 test_write <- data.frame(name = string_utf8,
                          stringsAsFactors = FALSE)
+# write UTF8 characters to fil
 # read UTF8 characters from file
-test_read <- read_tsv("./data/input/test_utf8.tsv")
+test_read <- read_tsv("../data/input/test_utf8.tsv")
 all(test_read$name == string_utf8)
 
 # text read Excel files with UTF-8 characters
-test_read_xl <- read_xlsx("./data/input/test_utf8.xlsx", 
+test_read_xl <- read_xlsx("../data/input/test_utf8.xlsx", 
                           sheet = "Blad1",
                           col_names = TRUE)
 all(test_read_xl$name == test_read$name)
