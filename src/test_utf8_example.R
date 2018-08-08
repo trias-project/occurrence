9b0764b2975bfb90f96aae78cd8335cb570a135b
damianooldoni <damiano.oldoni@inbo.be>
2018-08-08 09:28:43 +0200

    Addin extra tests with more UTF-8 characters

--- src/test_utf8_example.R
+++ src/test_utf8_example.R
@@ -5,12 +5,17 @@ library(readxl)
 Sys.getlocale(category = "LC_CTYPE")
 
 # read UTF-8 text files
-string_utf8 <- "¡I¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶"
+string_utf8 <- "¡I¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶ČŽÃÐč"
+string_utf8 <- iconv(string_utf8, "latin1", "UTF-8",sub='')
+string_utf8
+
+all_characters_utf8 <- "#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[ \ ]^_`abcdefghijklmnopqrstuvwxyz{|}~€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ ¡¢£¤¥¦§¨©ª«¬ ®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞ"
+Encoding(all_characters_utf8)
 test_write <- data.frame(name = string_utf8,
                          stringsAsFactors = FALSE)
 # write UTF8 characters to fil
 # read UTF8 characters from file
-test_read <- read_tsv("../data/input/test_utf8.tsv")
+test_read <- readLines("../data/input/test_utf8.tsv", encoding = 'UTF-8')
 all(test_read$name == string_utf8)
 
 # text read Excel files with UTF-8 characters
